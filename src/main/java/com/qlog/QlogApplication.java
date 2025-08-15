286ff1afe2e45ee3a16a4cec2112ffdd8fed2686
thr13 <skhrnt2945@gmail.com>
2025-08-15 17:33:22 +0900

    init

---
+++ src/main/java/com/qlog/QlogApplication.java
@@ -0,0 +1,13 @@
+package com.qlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qlog/QlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
