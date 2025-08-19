195ede8872a58843fb8cfdf28bb30869da74e558
thr13 <skhrnt2945@gmail.com>
2025-08-19 23:22:26 +0900

    feat: Profile 엔티티와 Repository

--- src/main/java/com/qlog/backend/user/domain/model/Profile.java
+++ src/main/java/com/qlog/backend/user/domain/model/Profile.java
@@ -16,26 +16,20 @@ public class Profile {
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user; //1:1 필수관계 (관계의 주인은 Profile)
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
+    @Setter
     private String nickname; //닉네임
 
-    @Column(name = "gender")
+    @Column(name = "gender", nullable = false)
     @Setter
     private Gender gender; //성별
 
-    @Column(length = 30)
+    @Column(length = 30, nullable = false)
     private String name; //이름
 
     public Profile() {
     }
 
-    //성별을 선택하지 않는 경우
-    public Profile(String nickname, String name) {
-        this.nickname = nickname;
-        this.name = name;
-    }
-
-    //성별이 포함된 경우
     public Profile(String nickname, Gender gender, String name) {
         this.nickname = nickname;
         this.gender = gender;
