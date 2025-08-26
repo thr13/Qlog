be85ecfe3311ed0768edb015c57c78a3182cc5c5
thr13 <skhrnt2945@gmail.com>
2025-08-26 23:57:15 +0900

    fix: Profile 엔티티 수정

--- src/main/java/com/qlog/backend/user/domain/model/Profile.java
+++ src/main/java/com/qlog/backend/user/domain/model/Profile.java
@@ -1,12 +1,21 @@
 package com.qlog.backend.user.domain.model;
 
+import com.qlog.backend.post.domain.model.CommentLike;
+import com.qlog.backend.post.domain.model.Post;
+import com.qlog.backend.post.domain.model.PostLike;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
 @Table(name = "profiles")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Profile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,6 +29,7 @@ public class Profile {
     @Setter
     private String nickname; //닉네임
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "gender", nullable = false)
     @Setter
     private Gender gender; //성별
@@ -27,8 +37,14 @@ public class Profile {
     @Column(length = 30, nullable = false)
     private String name; //이름
 
-    public Profile() {
-    }
+    @OneToMany(mappedBy = "profile")
+    private List<Post> posts = new ArrayList<>(); //Post 테이블과 1:N 관계, 관계의 주인은 Post
+
+    @OneToMany(mappedBy = "profile", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PostLike> postLikes = new ArrayList<>(); //1:N 게시글 추천
+
+    @OneToMany(mappedBy = "profile", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CommentLike> commentLikes = new ArrayList<>(); //1:N 댓글 추천
 
     public Profile(String nickname, Gender gender, String name) {
         this.nickname = nickname;
@@ -36,8 +52,21 @@ public class Profile {
         this.name = name;
     }
 
+    //프로필 업데이트
+    public void updateProfile(String nickname, String name) {
+        this.nickname = nickname;
+        this.name = name;
+    }
+
+    //닉네임 변경
+    public void updateNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    //User 와 양방향 관계
     public void setUser(User user) {
         this.user = user;
         user.setProfile(this);
     }
+
 }
