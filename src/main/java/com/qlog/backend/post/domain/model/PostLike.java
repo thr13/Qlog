257a29928436fc9a7c412a58cadb08497f6916f9
thr13 <skhrnt2945@gmail.com>
2025-08-26 23:57:32 +0900

    fix: Post, PostLike 엔티티 수정

--- src/main/java/com/qlog/backend/post/domain/model/PostLike.java
+++ src/main/java/com/qlog/backend/post/domain/model/PostLike.java
@@ -2,30 +2,33 @@ package com.qlog.backend.post.domain.model;
 
 import com.qlog.backend.user.domain.model.Profile;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @Table(name = "post_like")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostLike {
 
     @EmbeddedId
     private PostLikeId id; //게시글 추천
 
     @ManyToOne
+    @MapsId("profileId")
     @JoinColumn(name = "profile_id")
     private Profile profile;
 
     @ManyToOne
+    @MapsId("postId")
     @JoinColumn(name = "post_id")
     private Post post;
-
-    public PostLike() {
-    }
 
     public PostLike(Profile profile, Post post) {
         this.id = new PostLikeId(profile.getId(), post.getId());
         this.profile = profile;
         this.post = post;
     }
+
 }
--- src/main/java/com/qlog/backend/post/domain/model/PostLikeId.java
+++ src/main/java/com/qlog/backend/post/domain/model/PostLikeId.java
@@ -1,15 +1,20 @@
 package com.qlog.backend.post.domain.model;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
+@EqualsAndHashCode
 @Embeddable
-public class PostLikeId {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostLikeId implements Serializable {
 
     private Long profileId; //프로필 ID
-    private Long postId; //게시글 ID
 
-    public PostLikeId() {
-    }
+    private Long postId; //게시글 ID
 
     public PostLikeId(Long profileId, Long postId) {
         this.profileId = profileId;
