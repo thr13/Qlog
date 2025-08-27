5115b4724452d9d7e433b989a7ecdb153985c147
thr13 <skhrnt2945@gmail.com>
2025-08-27 11:17:08 +0900

    feat: UserRepository 이메일로 조회 추가

--- src/main/java/com/qlog/backend/user/domain/repository/UserRepository.java
+++ src/main/java/com/qlog/backend/user/domain/repository/UserRepository.java
@@ -3,7 +3,15 @@ package com.qlog.backend.user.domain.repository;
 import com.qlog.backend.user.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    /**
+     * 이메일로 사용자 조회
+     * @param email 사용자 이메일
+     * @return Optional 로 감싼 User 객체
+     */
+    Optional<User> findByEmail(String email);
 }
