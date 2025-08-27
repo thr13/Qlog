2834d04476286f89f7e3612dc6b17040b0770cd2
thr13 <skhrnt2945@gmail.com>
2025-08-27 11:18:05 +0900

    docs: ProfileRepository 메서드 주석 추가

--- src/main/java/com/qlog/backend/user/domain/repository/ProfileRepository.java
+++ src/main/java/com/qlog/backend/user/domain/repository/ProfileRepository.java
@@ -8,5 +8,10 @@ import java.util.UUID;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 
+    /**
+     * 사용자 식별키로 해당 사용자의 프로필 조회
+     * @param userId 사용자 식별키
+     * @return Optional 로 감싼 Profile 객체
+     */
     Optional<Profile> findByUser_Id(UUID userId);
 }
