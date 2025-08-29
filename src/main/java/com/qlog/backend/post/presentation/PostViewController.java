ed898999b843bce60b72c65850cbc8d4db98bda3
thr13 <skhrnt2945@gmail.com>
2025-08-30 00:01:34 +0900

    feat: 게시글 목록 조회 페이지

--- src/main/java/com/qlog/backend/post/presentation/PostViewController.java
+++ src/main/java/com/qlog/backend/post/presentation/PostViewController.java
@@ -3,6 +3,9 @@ package com.qlog.backend.post.presentation;
 import com.qlog.backend.post.application.PostService;
 import com.qlog.backend.post.presentation.dto.response.PostResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +20,27 @@ public class PostViewController {
     private final PostService postService;
 
     /**
+     * 게시글 목록 조회 페이지
+     * localhost:/8080/posts
+     *
+     * @param pageable 페이지 정보
+     * @param model    html 에 사용할 데이터
+     * @return resources/templates/post/list.html
+     */
+    @GetMapping
+    public String postList(@PageableDefault(sort = "createdAt") Pageable pageable, Model model) {
+        Page<PostResponse> postList = postService.getPostList(pageable);
+        model.addAttribute("postList", postList);
+
+        return "post/list";
+    }
+
+    /**
      * 게시글 상세 조회 페이지
      * localhost:8080/posts/{postId}
+     *
      * @param postId 게시글 ID
-     * @param model html 에 사용할 데이터
+     * @param model  html 에 사용할 데이터
      * @return resources/templates/post/detail.html
      */
     @GetMapping("/{postId}")
@@ -30,4 +50,5 @@ public class PostViewController {
 
         return "post/detail";
     }
+
 }
